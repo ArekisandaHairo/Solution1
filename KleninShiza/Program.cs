2f3b69a77cd862f106a671767a197a5362d3e162
Hairo <sanpopovich73@gmail.com>
2022-03-30 21:45:20 +1000

    30.03.2022

--- KleninShiza/Program.cs
+++ KleninShiza/Program.cs
@@ -48,125 +48,25 @@ namespace KleninShiza
                 // Alt + X и мы меняем окно
                 if ((keyInfo.Modifiers & ConsoleModifiers.Alt)!=0)
                 {
-                    if (keyInfo.Key == ConsoleKey.X)
+                    switch (keyInfo.Key)
                     {
-                        work._activeWin--;
-                        work.Swap(work._activeWin, work._list.Count-1 );
-                        work.PrintWin();
-                    }
-                    if (keyInfo.Key == ConsoleKey.V)
-                    {
-                        work.AddOkno();
-                    }
-                }
-            }
-            
-
-
-/*
- // ло.
-            while (true)
-            {
-                int index = 0;
-                ConsoleKeyInfo s = Console.ReadKey();
-                if (s.Key == ConsoleKey.Q)
-                {
-                    
-                }Я пока не понимаю зачем это тут... но оно работает, точнее, работа
-
-                if (s.Key == ConsoleKey.Q)
-                {
-                    
-                }
-
-                if (s.Key == ConsoleKey.Q)
-                {
-                    
-                }
-
-                if (s.Key == ConsoleKey.Q)
-                {
-                    
-                }
-
-                if (s.Key == ConsoleKey.NumPad6)
-                {
-                    windows[index].PeremesvhenieXUP();
-                }
-                if (s.Key == ConsoleKey.NumPad4)
-                {
-                    
-                    windows[index].PeremesvhenieXDOWN();
-                }
-                if (s.Key == ConsoleKey.NumPad8)
-                {
-                    
-                    windows[index].PeremesvhenieYUP();
-                }
-                if (s.Key == ConsoleKey.NumPad2)
-                {
-                    
-                    windows[index].PeremesvhenieYDOWN();
-                }
-                
-                if (s.Key == ConsoleKey.F1)
-                {
-                    for (int i = 0; i < windows.Count; i++)
-                    {
-                        if (i != 1)
-                        {
-                            windows[i].Draw();
-                        }
+                        case ConsoleKey.X:
+                            work._activeWin--;
+                            work.Swap(work._activeWin, work._list.Count-1 );
+                            work.PrintWin();
+                            break;
+                        case ConsoleKey.V:
+                            work.AddOkno();
+                            break;
                     }
                 }
-                if (s.Key == ConsoleKey.F2)
-                {
-                    for (int i = 0; i < windows.Count; i++)
-                    {
-                        if (i != 2)
-                        {
-                            windows[i].Draw();
-                        }
-                    }
-                }
-            }
-            */
-            //window.Draw();
-            //Window window = new Window(0, 0, Console.WindowWidth-1, Console.WindowHeight-1, official, "RAZRAB");
-            //Window num1 = new Window(posX,posY,40,15,header,"Okno1");
-            //Window num2 = new Window( 30,5,45,15,header,"Okno2");
-            //num1.Draw();
-            //num2.Draw();
+            } 
             /*
-            while (true)
-            {
-                string s;
-                Console.SetCursorPosition(1,Console.WindowHeight-2);
-                Console.Write("$user>>");
-                s = Console.ReadLine();
-                if (s == "Close")
-                {
-                    Console.SetCursorPosition(1,Console.WindowHeight-2);
-                    Console.Write("$user>>Close>>");
-                    s = Console.ReadLine();
-                    if (s == "Okno1")
-                    {
-                        Console.SetCursorPosition(1,Console.WindowHeight-2);
-                        Console.Write("$user>>Close>>Okno1");
-                    }
-                    for (int i = 0; i < windows.Count; i++)
-                    {
-                        if (s == "Okno1" && i != 1)
-                        {
-                            windows[i].Draw();
-                        }
-                    }
-
-                    
-                }
-                Console.SetCursorPosition(1,Console.WindowHeight-2);
-                Console.Write("$user>>");
-            }
+            * 3 2 1 0 
+            * 2 3 1 0
+            * 2 1 3 0
+            * 2 1 0 3
+            * 3 2 1 0
             */
         }
     }
